82bdf400e466ee9941700558abf801dc17256b99
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-08-15 23:45:39 -0600

    calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def sum(arr)
+    counter = 0
+    arr.each { |number| counter += number  }
+    return counter
+end
+
+def multiply(arr)
+    counter = 1
+    arr.each { |number| counter *= number  }
+    return counter
+end
+
+def power(n)
+    return n**2
+end 
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -77,16 +77,22 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+      expect(multiply([3,4])).to eq(12)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+      expect(multiply([1,2,3,4,5])).to eq(120)
+  end
+
 end
 
-describe "#power" do
-  it "raises one number to the power of another number"
+describe "power" do
+  it "raises one number to the power of another number" do
+      expect(power(2)).to eq(4)
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
